949118711d3e4e0f25cb649864655de51d24efbc
iCHIrTmA <jonrue.dev@gmail.com>
2024-11-19 05:14:45 +0800

    feat: added constants

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+# import everything from a module
+# into the current file
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
